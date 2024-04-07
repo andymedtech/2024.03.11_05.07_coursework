3d12cb4bf70cef71e91642ffafe21d3279650766
Andrey Belomyttsev <andymedtech@gmail.com>
2024-04-07 18:06:01 +0300

    init: cors

--- modules/HTTPServer.js
+++ modules/HTTPServer.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = require("../routes");
+const cors = require("cors"); // frontend
 
 const app = express();
+
+// frontend cors для совместимости адресов (добавили библиотеку)
+app.use(cors()); // frontend
 
 app.use(express.json()); // расшифровка JSON
 // app.use(express.urlencoded()); // расшифровка URL
@@ -9,7 +13,7 @@ app.use("/api", router); // префикс к маршуту (good practice)
 
 // frontend: express static folder
 // http://localhost:8080/index.html
-app.use(express.static("public"));
+app.use(express.static("public")); // frontend
 
 // порт по умолчанию 8080, если не передаётся другой из /index.js
 const run = (port = 8080) => {
